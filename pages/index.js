e2b686d5597ebba8b2effa702710e6e1f953e9cb
Dragos Apostol <dragosapostol@Dragoss-MacBook-Pro.local>
2022-07-24 16:46:00 +0300

    remove tests

--- pages/index.js
+++ pages/index.js
@@ -121,7 +121,7 @@ const Home = () => {
                     Adresa: Galati, Calea Smardan 99 (In spatele fabricii ARCADA
                     Filesti)
                     <br />
-                    <span className="flex flex-row">
+                    <span className="flex flex-row items-center">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
@@ -130,8 +130,14 @@ const Home = () => {
                       >
                         <path d="M12 2c3.196 0 6 2.618 6 5.602 0 3.093-2.493 7.132-6 12.661-3.507-5.529-6-9.568-6-12.661 0-2.984 2.804-5.602 6-5.602m0-2c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
                       </svg>
-                      <Link href="https://goo.gl/maps/GDt1LaeCBn7VXSDt6">
-                        Apasati pentru Locatie
+                      <Link
+                        href="https://goo.gl/maps/GDt1LaeCBn7VXSDt6"
+                        // className="text-blue-700"
+                        className="nav-link"
+                      >
+                        <span className="text-blue-700">
+                          Apasati pentru Locatie
+                        </span>
                       </Link>
                     </span>
                   </p>
